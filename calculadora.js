70a12943b960d4838da4ab7dfc1a55fe7702d146
adrianagers <adrianageraldine.19@gmail.com>
2020-09-10 16:42:18 -0500

    Calculadora -onclick, function v1

---
+++ calculadora.js
@@ -0,0 +1,157 @@
+// alert('Hola Agers como tas tengo que volver con mas ejercicios de c++'); para alertas 
+// console.log("mensaje para la consola del equipo");se muestra mensajes en la consola
+// con el DOM se puede hace cosas como si fuera el html
+
+// document.write("calculadora");para remplazar cosas en el html 
+// prompt("ingrese su nombre"); para que ingresedatos 
+
+// cracion de variables  diferentes formasa
+// let=> para uso local
+// // var=> alcance es global 
+
+// var age= 22;
+// let edad=15;
+
+// // funciones 
+// function sumaNumeros(){
+//     // var age='geraldine';
+//     // let edad=30;
+//     console.log(edad+ege);
+// }
+
+// sumaNumeros();
+
+// function sumarEdad(){
+//     let edad=prompt("ingresa tu edad");
+//     let nuevaEdad=parseInt(edad)+10;
+//     alert(nuevaEdad);
+// }
+// sumarEdad();
+
+// function sumaDosNum(){
+//     let num1 = parseInt(prompt("ingresa el primer numero"));
+//     let num2 = parseInt(prompt("ingrese el segundo numero"));
+//     let respt = num1 + num2;
+//     alert("Su resultado es " + respt)
+// }
+// sumaDosNum();
+
+// function cambiarMensaje(idDIV){
+// let nuevaFrse= prompt("escribe lo que desees");
+// document.getElementById(idDIV).innerHTML = nuevaFrse;
+// }
+
+// innerHTML= div, h, p, button, Li
+// pero si queremos modificar el input hacemos ('data').value=numero
+
+
+// condicionales
+// nos permite validar si algo es cierto o no 
+
+
+
+
+
+// cambiarMensaje();
+function pintarNumero(numero) {
+  //  document.getElementById('data').value=numero;
+  pintarDato(numero);
+}
+
+function pintaroperador(operador) {
+  // document.getElementById('data').value=operador;
+  pintarDato(` ${operador} `);
+}
+
+function pintarDato(dato) {
+  let valorInput = document.getElementById('data')
+  //  valorInput.value = valorInput.value + dato;
+  valorInput.value = `${valorInput.value}${dato}`
+}
+
+function LimpiarDatos() {
+  document.getElementById('data').value = '';
+}
+
+function respDatos() {
+  let valorInput = document.getElementById('data').value;
+  let arreglo = valorInput.split(' ')
+
+  // if (arreglo[1] == '+') {
+  //   let suma = parseInt(arreglo[0]) + parseInt(arreglo[2]);
+  //   document.getElementById('data').value = suma;
+
+  // } else if (arreglo[1] == '-') {
+  //   let resta = parseInt(arreglo[0]) - parseInt(arreglo[2]);
+  //   document.getElementById('data').value = resta;
+  // }
+  // else if (arreglo[1] == '$') {
+  //   let multi = parseInt(arreglo[0]) * parseInt(arreglo[2]);
+  //   document.getElementById('data').value = multi;
+  // }
+  // else {
+  //   let divi = parseInt(arreglo[0]) / parseInt(arreglo[2]);
+  //   document.getElementById('data').value = divi;
+  // }
+  switch(arreglo[1]){
+    case '+':
+      let suma = parseInt(arreglo[0]) + parseInt(arreglo[2]);
+      document.getElementById('data').value = suma;
+      break
+    case '-':
+      let resta = parseInt(arreglo[0]) - parseInt(arreglo[2]);
+      document.getElementById('data').value = resta;
+      break
+    case '*':
+      let multi = parseInt(arreglo[0]) * parseInt(arreglo[2]);
+      document.getElementById('data').value = multi;
+      break
+    default:
+      let divi = parseInt(arreglo[0]) / parseInt(arreglo[2]);
+      document.getElementById('data').value = divi;
+  }
+}
+function mostraMensaje(){
+  let DiaQDeseas= prompt("escribe lo que desees");
+  
+switch(DiaQDeseas){
+  case 'Lunes':
+    case 'lunes': 
+        case '1': 
+        alert(" Exelente dia hoy es lunes :)");
+  break
+  case 'Martes':
+    case 'martes':
+      case '2':alert(" Exelente dia hoy es Martes :)");
+    break
+  case 'Miercoles':
+    case 'miercoles':
+      case '3':alert(" Exelente dia hoy es Miercoles :)");
+    break
+    case 'Jueves':
+      case 'jueves':
+        case '4':alert(" Exelente dia hoy es Jueves :)");
+    break
+    case 'Viernes':
+      case 'viernes':
+        case '5':alert(" Exelente dia hoy es viernes :)");
+      break
+    case 'Sabado':
+      case 'sabado':
+        case '6':alert(" Exelente dia hoy es Sabado :)");
+      break
+      case 'Domingo':
+        case 'domingo':
+          case '7':alert(" Exelente dia hoy es Domingo :) ");
+      break
+  default:
+   alert(" Lo sentimos ese dia no esta identificado ");
+}
+}
+// let diasAño= prompt("ingrese el numero que desees y te dire el año ");
+
+// if(diasAño == 6 ){
+//   alert("este mes es junio");
+// }else{
+//  alert("nuevamente ingrese un numero")
+// }
